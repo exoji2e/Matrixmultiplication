1baa39d73133e0e070c5bfcba68e597bd96f1f49
max614 <maax614@gmail.com>
2016-04-19 15:37:24 +0200

    100 iterations

--- perfKaratsuba.java
+++ perfKaratsuba.java
@@ -1,7 +1,12 @@
 import java.util.Random;
 public class perfKaratsuba {
 	public static void main(String args[]) {
-		for (int n = 0; n <= 17; n++){
+
+
+		for (int n = 1; n <= 17; n++){
+			long tk = 0;
+			long tm = 0;
+			for (int i = 0 ; i < 100 ; i++){
 			int deg1 = (int)Math.pow(2, n);
 			int deg2 = (int)Math.pow(2, n);
 			Random r = new Random();
@@ -15,20 +20,29 @@ public class perfKaratsuba {
 			}
 			Polynomial p1 = new Polynomial(koef1);
 			Polynomial p2 = new Polynomial(koef2);
+
 			long t1 = System.nanoTime();
 			Polynomial pkar=p2.karatsuba(p1);
 			t1 = System.nanoTime() - t1;
 			long t2 = System.nanoTime();
 			Polynomial pmult = p2.multiply(p1);
 			t2 = System.nanoTime() - t2;
-			double ratio = ((double) t2)/((double) t1);
-			System.out.println(n + " " + ratio + " " + t1 + " " + t2);
+			tk += t1;
+			tm += t2;
+		}
+		double ratio = ((double) tm)/((double) tk);
+		System.out.println(n + " " + ratio + " " + tk/100 + " " + tm/100);
+
+
+
+
 			/*System.out.println("Degree: 2^" + n + " pkar.equals(pmul):" + pkar.equals(pmult));
 			System.out.println("multiply/karatsuba = " + ratio);
 			System.out.println("karatsuba = " + t1);
 			System.out.println("multiply = " + t2);
-			
+
 			System.out.println("---------");*/
 		}
+
 	}
 }
